dd25c64158032294e05333c4cd49929c0f677267
リギョウメイ <li_x@mti.co.jp>
2017-05-12 16:06:17 +0900

    add applytr

---
+++ src/main/java/jp/co/hyron/ope/common/CommonConst.java
@@ -0,0 +1,17 @@
+package jp.co.hyron.ope.common;
+
+public class CommonConst {
+
+    // システム管理権限
+    public static final String DEF_AUTHOR_ROLE_ADMIN = "ROLE_ADMIN";
+
+    // 上位管理者権限
+    public static final String DEF_AUTHOR_ROLE_MANAGER = "ROLE_MANAGER";
+
+    // 一般権限
+    public static final String DEF_AUTHOR_ROLE_NORMAL_USER = "ROLE_NORMAL_USER";
+
+    // 事務処理権限
+    public static final String DEF_AUTHOR_ROLE_JIMU = "ROLE_JIMU";
+
+}
